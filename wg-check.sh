674a0e1e4fe30f94cb579e71ac5876722f352782
LockeJet <33934004+LockeJet@users.noreply.github.com>
2021-11-30 17:42:19 +0800

    Update wg-check.sh

--- wg-check.sh
+++ wg-check.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-ping -c1 [edit it to your peer ip] > /dev/null
+TUN_IP=10.5.0.1
+WG_IFACE=wgs0
+WG_SERVICE=wg-quick@wgs0.service
+
+ping -c 2 -w 3 $TUN_IP > /dev/null
 if [ $? -eq 0 ]
-  then 
+  then
     exit 0
   else
-    wg-quick down wg0;
-    wg-quick up wg0;
+    #wg-quick down $WG_IFACE;
+    #wg-quick up $WG_IFACE;
+    systemctl restart $WG_SERVICE
 fi
