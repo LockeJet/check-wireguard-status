be5f79765a403f47c1fedbcc58ed650076d3f900
LockeJet <33934004+LockeJet@users.noreply.github.com>
2021-11-30 17:45:40 +0800

    Update wg-check.sh

--- wg-check.sh
+++ wg-check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 TUN_IP=10.5.0.1
 WG_IFACE=wgs0
 WG_SERVICE=wg-quick@wgs0.service
@@ -8,7 +9,5 @@ if [ $? -eq 0 ]
   then
     exit 0
   else
-    #wg-quick down $WG_IFACE;
-    #wg-quick up $WG_IFACE;
     systemctl restart $WG_SERVICE
 fi
